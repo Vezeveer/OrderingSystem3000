33a073ee6b98d9d873f112e1867f05ca526189f4
vezeveer <mortalmanny@live.com>
2020-03-04 21:07:06 +0800

    improved add remove funtionality

--- OrderingSystem3000/Program.cs
+++ OrderingSystem3000/Program.cs
@@ -9,24 +9,23 @@ namespace OrderingSystem3000
     class Program
     {
         static int[] cart = { 0, 0, 0 };
-        static int[] stock = { 200, 150, 400 };
+        static int[] stockAvailable = { 200, 150, 400 };
         static double[] price = { 52.00, 99.00, 34.00 };
         static string[] labels = { "apple", "mango", "banana" };
         static double dueCost = 0.00;
 
         static string homeTitle = "Welcome, user!";
-        static string[] homeMenu = {
+        static string[] homeMenuOptions = {
             "1. Browse Items",
             "2. Check Cart & Checkout",
             "3. Exit" };
-        static int homeChoice, browseChoice, itemChoice;
         static string browseTitle = "Choose your product";
-        static string[] browseItems = {
+        static string[] productOptions = {
             "1. Apple",
             "2. Mango",
             "3. Banana",
             "4. Go Back"};
-        static string[] itemMenu = {
+        static string[] addRemoveOptions = {
             "1. Add",
             "2. Remove",
             "3. Go Back" };
@@ -37,38 +36,60 @@ namespace OrderingSystem3000
             
             do
             {
-                renderScreen(homeTitle, homeMenu); //home selection menu
-                homeChoice = checkChoice(homeTitle, homeMenu, 3);
-                
-                switch (homeChoice) 
+                renderScreen(homeTitle, homeMenuOptions); //home options menu
+                switch (checkChoice(homeTitle, homeMenuOptions, 3)) 
                 {
-                    case 1: //product selection menu
+                    case 1: //product options
                         do
                         {
-                            renderScreen(browseTitle, browseItems);
-                            browseChoice = checkChoice(browseTitle, browseItems, 4);
-                            switch (browseChoice) 
+                            renderScreen(browseTitle, productOptions);
+
+                            switch (checkChoice(browseTitle, productOptions, 4)) 
                             {
-                                case 1: 
-                                    addDeleteSelectMenu("Apples");
+                                case 1: //add or remove options for selected product
+                                    bool exitLoop = false;
+                                    do
+                                    {
+                                        renderScreen("Apples", addRemoveOptions);
+                                        switch(checkChoice("Apples", addRemoveOptions, 3))
+                                        {
+                                            case 1:
+                                                //add
+                                                addToCart(0, "Apples");
+                                                exitLoop = true;
+                                                break;
+                                            case 2:
+                                                //delete
+                                                removeFromCart(0, "Apples");
+                                                exitLoop = true;
+                                                break;
+                                            case 3:
+                                                //go back
+                                                exitLoop = true;
+                                                break;
+                                        }
+                                        if (exitLoop) break;
+                                    } while (true);
                                     break;
                                 case 2:
-                                    addDeleteSelectMenu("Mangos");
+                                    
                                     break;
                                 case 3:
-                                    addDeleteSelectMenu("Bananas");
+                                    
+                                    break;
+                                case 4: //Go Back
+                                    exitBrowseChoice = true; 
                                     break;
                                 default:
-                                    exitBrowseChoice = true; //Go Back
                                     break;
                             }
                             if (exitBrowseChoice) break;
                         } while (true);
                         
                         break;
-                    case 2:
+                    case 2: //Checkout
                         break;
-                    case 3:
+                    case 3: //Exit
                         loop = false;
                         break;
                 }
@@ -84,26 +105,35 @@ namespace OrderingSystem3000
 
         }
 
-        static void addDeleteSelectMenu(string product)
+        static void addToCart(int id, string name)
         {
-            renderScreen(product, itemMenu);
-            itemChoice = checkChoice(product, itemMenu, 3);
-
-            switch (itemChoice)
+            string[] addMenu = { "How many would you like to add?",
+                                                        "Available stock: " + stockAvailable[id],
+                                                        "In Cart: " + cart[0]};
+            if (stockAvailable[id] == 0)
             {
-                case 1:
-                    renderScreen("Adding " + product, null);
-                    int amount = checkChoice("Apples to add", new string[] { "Amount to add" }, stock[0]);
-                    Console.WriteLine("Success");
-                    Console.ReadLine();
-                    addToCart(0, amount);
-                    break;
-                default:
-                    break;
+                renderScreen("Sorry", new string[] { "Nothing in stock today!" });
+                Console.WriteLine("Press any key to go back...");
+                Console.ReadLine();
+            }
+            else
+            {
+                renderScreen("Adding " + name, addMenu);
+                int addAmount = checkChoice("Adding" + name,
+                    addMenu,
+                    stockAvailable[id]);
+
+                cart[id] += addAmount;
+                stockAvailable[id] -= addAmount;
+
+                renderScreen("Success!", new string[] { "Added " + addAmount + " " + name + " to cart." });
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadLine();
             }
         }
 
-        //returns a choice
+
+        //gets input, returns a choice
         static int checkChoice(string title, string[] menu, int maxChoices)
         {
             int choice;
@@ -146,14 +176,28 @@ namespace OrderingSystem3000
             Console.WriteLine("||\n");
         }
 
-        static void addToCart(int itemID, int amount)
+        static void removeFromCart(int id, string name)
         {
-            cart[itemID] += amount;
-        }
+            string[] deleteMenu = { "How many do you want to remove?", name + " in cart: " + cart[id] };
+            if (cart[id] == 0)
+            {
+                renderScreen("Oops", new string[] { "Nothing in your cart to delete!" });
+                Console.WriteLine("Press any key to go back...");
+                Console.ReadLine();
+            }
+            else
+            {
+                renderScreen(name + " Removing", deleteMenu);
+                int deleteAmount = checkChoice(name + " Removing", deleteMenu, cart[id]);
 
-        static void removeFromCart(int itemID, int amount)
-        {
-            cart[itemID] -= amount;
+                cart[id] -= deleteAmount;
+                stockAvailable[id] += deleteAmount;
+
+                renderScreen("Success!", new string[] { "Removed " + deleteAmount + " " + name + " from cart." });
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadLine();
+            }
+            
         }
     }
 }
